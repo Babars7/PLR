51952ac83da3524c4b4d7d2de5af867b17075622
loinicola <loinicola98@gmail.com>
2021-05-06 20:16:31 +0200

    TEST

--- real-lsd/agents/ppo_agent/lander.py
+++ real-lsd/agents/ppo_agent/lander.py
@@ -32,7 +32,7 @@ threshold_reward = -200
 def save_obj(obj, filename):
     dir = 'data'
     # path = os.getcwd()
-    PATH = '/home/plr/PLR'    # '/media/scratch2/plr_project/PLR'
+    PATH = '/media/scratch2/plr_project/PLR'  #'/home/plr/PLR'    # '/media/scratch2/plr_project/PLR'
 
     if dir not in os.listdir(PATH):
         PATH =  os.path.join(PATH, dir)
@@ -92,7 +92,7 @@ log.setLevel("WARN")
 
 # Copy settings file to data folder
 abs_path = os.path.dirname(real_lsd.__file__) + '/envs/settings/landing/gorner.json'
-cp_path  = '/home/plr/PLR/data/json' # '/home/nicloi/plr_project/data'
+cp_path  = '/home/nicloi/plr_project/data' #'/home/plr/PLR/data/json' # '/home/nicloi/plr_project/data'
 list_files = subprocess.run(["cp", abs_path, cp_path])
 log.warn("The exit code was: %d" % list_files.returncode)
 
